64b5b84411a0de9dee44c284c6d56dfad7d91943
NumanKilincoglu <numan8114@hotmail.com>
2024-05-15 18:55:03 +0300

    test

--- controller/socket-controller.js
+++ controller/socket-controller.js
@@ -197,6 +197,13 @@ io.on("connect", (socket) => {
   });
 });
 
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
 function getUserIdFromSocket(socket) {
   for (const [userID, socketID] of connectedUsers.entries()) {
     if (socketID === socket.id) {
